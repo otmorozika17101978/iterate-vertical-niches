df50d943d2b16ab548cef3bb93a040752b06c1cf
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-09-22 03:59:21 +0300

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,30 +1,46 @@
-function generateRandomData() {
 }
+    });
+    const data = [];
+    }
+function generateRandomData() {
+
+
+main();
+    data.forEach(item => {
     const data = generateRandomData();
+}
+        console.log("Random Number: " + item);
+    return data;
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+function main() {
+    for (let i = 0; i < 10; i++) {function main() {
+        console.log("Random Number: " + item);
+    }
 
 }
-    }
-        data.push(Math.floor(Math.random() * 100) + 1);
-    const data = [];
-    data.forEach(item => {
-function main() {
-        console.log("Random Number: " + item);
-    for (let i = 0; i < 10; i++) {
-main();
     });
 
+    data.forEach(item => {
+    const data = [];
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
     return data;
+function generateRandomData() {
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+main();
 
     main()
-if __name__ == "__main__":
-    for item in data:
-
-    data = generate_random_data()
-    return data
-        print(f"Random Number: {item}")
     data = [random.randint(1, 100) for _ in range(10)]
+    return data
 def main():
 
+    for item in data:
 
-import random
+        print(f"Random Number: {item}")
 def generate_random_data():
+if __name__ == "__main__":
+import random
+    data = generate_random_data()
